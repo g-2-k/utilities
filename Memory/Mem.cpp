0e4434c09b958d690dad2c94e6121896e197311d
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-11-12 12:36:14 +0530

    Using interim buffer to write instead of directly to memory

--- Memory/Mem.cpp
+++ Memory/Mem.cpp
@@ -44,23 +44,19 @@ void mem::write(unsigned int dat, signed int bln)
 		bln -= (8-iBit);
 		if(bln > 0)
 		{
-//			buffer |= (unsigned char)(dat >> bln);
-			*ptr |= (unsigned char)(dat >> bln);
+			buffer |= (unsigned char)(dat >> bln);
 			iBit = 8;
 		}
 		else
 		{
-//			buffer |= (unsigned char)(dat << (-1*bln));
-			*ptr |= (unsigned char)(dat << (-1*bln));
+			buffer |= (unsigned char)(dat << (-1*bln));
 			iBit = 8+bln;
 		}
-//		*ptr = buffer;
-//		buffer = 0;
 		if(iBit == 8)
 		{
-//			*ptr = buffer;
-				ptr++;
-//				buffer = 0;
+			*ptr = buffer;
+			ptr++;
+			buffer = 0;
 			iBit = 0;
 		}
 	}
